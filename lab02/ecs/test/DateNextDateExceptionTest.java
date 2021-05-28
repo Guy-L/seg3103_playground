070359cea6603d7230a22987047f61896e81057d
Guy L <g.guy.lab@gmail.com>
2021-05-27 23:54:59 -0400

    Finished q2

--- lab02/ecs/test/DateNextDateExceptionTest.java
+++ lab02/ecs/test/DateNextDateExceptionTest.java
@@ -1,5 +1,43 @@
-public class DateNextDateExceptionTest
-{
+import java.util.LinkedList;
+import java.util.List;
 
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
+@RunWith(Parameterized.class)
+public class DateNextDateExceptionTest {
+	
+	private int year;
+	private int month;
+	private int day;
+
+	public DateNextDateExceptionTest(int year, int month, int day){
+		//Note: since system constraints make it impossible to test the "nextDay" method
+		//with an invalid date, we're not so much testing this method as we are testing if
+		//these test cases are of valid dates. This is reflected in the below method name.
+		
+		this.year  = year;
+		this.month = month;
+		this.day   = day;
+	}
+
+	@Parameters
+	public static List<Integer[]> data() {
+		List<Integer[]> params = new LinkedList<Integer[]>();
+
+		params.add(new Integer[] {1500, 2,  31});
+		params.add(new Integer[] {1500, 2,  29});
+		params.add(new Integer[] {-1,   10, 20});
+		params.add(new Integer[] {1458, 15, 12});
+		params.add(new Integer[] {1975, 6, -50});
+		return params;
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testIsValidDate(){ 
+		new Date(year, month, day);
+	}
 }
--- lab02/ecs/test/DateTest.java
+++ lab02/ecs/test/DateTest.java
@@ -1,13 +1,105 @@
 import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
 
 class DateTest {
-
-  @Test
-  void nextDate_sample() {
-    Date d = new Date(2020,5,3);
-    assertEquals(new Date(2020,5,4), d.nextDate());
-  }
-
+	
+	@Test
+	void testcase_1() {
+		assertEquals(new Date(1700, 6, 20).nextDate(), new Date(1700, 6, 21));
+	}
+	
+	@Test
+	void testcase_2() {
+		assertEquals(new Date(2005, 4, 15).nextDate(), new Date(2005, 4, 16));
+	}
+	
+	@Test
+	void testcase_3() {
+		assertEquals(new Date(1901, 7, 20).nextDate(), new Date(1901, 7, 21));
+	}
+	
+	@Test
+	void testcase_4() {
+		assertEquals(new Date(3456, 3, 27).nextDate(), new Date(3456, 3, 28));
+	}
+	
+	@Test
+	void testcase_5() {
+		assertEquals(new Date(1500, 2, 17).nextDate(), new Date(1500, 2, 18));
+	}
+	
+	@Test
+	void testcase_6() {
+		assertEquals(new Date(1700, 6, 29).nextDate(), new Date(1700, 6, 30));
+	}
+	
+	@Test
+	void testcase_7() {
+		assertEquals(new Date(1800, 11, 29).nextDate(), new Date(1800, 11, 30));
+	}
+	
+	@Test
+	void testcase_8() {
+		assertEquals(new Date(3453, 1, 29).nextDate(), new Date(3453, 1, 30));
+	}
+	
+	@Test
+	void testcase_9() {
+		assertEquals(new Date(444, 2, 29).nextDate(), new Date(444, 3, 1));
+	}
+	
+	@Test
+	void testcase_10() {
+		assertEquals(new Date(2005, 4, 30).nextDate(), new Date(2005, 5, 1));
+	}
+	
+	@Test
+	void testcase_11() {
+		assertEquals(new Date(3453, 1, 30).nextDate(), new Date(3453, 1, 31));
+	}
+	
+	@Test
+	void testcase_12() {
+		assertEquals(new Date(3456, 3, 30).nextDate(), new Date(3456, 3, 31));
+	}
+	
+	@Test
+	void testcase_13() {
+		assertEquals(new Date(1901, 7, 31).nextDate(), new Date(1901, 8, 1));
+	}
+	
+	@Test
+	void testcase_14() {
+		assertEquals(new Date(3453, 1, 31).nextDate(), new Date(3453, 2, 1));
+	}
+	
+	@Test
+	void testcase_15() {
+		assertEquals(new Date(3456, 12, 31).nextDate(), new Date(3457, 1, 1));
+	}
+	
+	@Test
+	void testcase_16() {
+		assertThrows(IllegalArgumentException.class, () -> new Date(1500, 2, 31));
+	}
+	
+	@Test
+	void testcase_17() {
+		assertThrows(IllegalArgumentException.class, () -> new Date(1500, 2, 29));
+	}
+	
+	@Test
+	void testcase_18() {
+		assertThrows(IllegalArgumentException.class, () -> new Date(-1, 10, 20));
+	}
+	
+	@Test
+	void testcase_19() {
+		assertThrows(IllegalArgumentException.class, () -> new Date(1458, 15, 12));
+	}
+	
+	@Test
+	void testcase_20() {
+		assertThrows(IllegalArgumentException.class, () -> new Date(1975, 6, -50));
+	}
 }
