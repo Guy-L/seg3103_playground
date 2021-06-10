8ab0ba29e4d6e8216b26714c7acf4c0334c85bfe
Guy L <g.guy.lab@gmail.com>
2021-06-09 22:32:39 -0400

    Added desire: testOppositeOfNegatives

--- lab04/calculator/src/CalculatorTest.java
+++ lab04/calculator/src/CalculatorTest.java
@@ -23,5 +23,15 @@ class CalculatorTest {
 		assertEquals(Calculator.opposite(10000), -1);
 		assertEquals(Calculator.opposite((int)00093f), -39);
 	}
+	
+	@Test
+	void testOppositeOfNegatives() {
+		assertEquals(Calculator.opposite(-6), 6);
+		assertEquals(Calculator.opposite(-51), 15);
+		assertEquals(Calculator.opposite(-154), 451);
+		assertEquals(Calculator.opposite(-307), 703);
+		assertEquals(Calculator.opposite(-10000), 1);
+		assertEquals(Calculator.opposite(-(int)00093f), 39);
+	}
 
 }
