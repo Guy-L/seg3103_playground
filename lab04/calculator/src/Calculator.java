4403b6f62d7dba7a27e22c24954cb84e9faa888c
Guy L <g.guy.lab@gmail.com>
2021-06-09 22:13:07 -0400

    Added desire: testOppositeZero

---
+++ lab04/calculator/src/Calculator.java
@@ -0,0 +1,18 @@
+public class Calculator {
+	
+	public static float opposite(float n) {
+		throw new RuntimeException("Stub");
+	}
+	
+	public static float sum(float lhs, float rhs) {
+		throw new RuntimeException("Stub");
+	}
+	
+	public static float max(float lhs, float rhs) {
+		throw new RuntimeException("Stub");
+	}
+	
+	public static float multiply(float lhs, float rhs) {
+		throw new RuntimeException("Stub");
+	}
+}
---
+++ lab04/calculator/src/CalculatorTest.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CalculatorTest {
+
+	@Test
+	void testOppositeZero() {
+		assertEquals(Calculator.opposite(0), 0);
+	}
+
+}
